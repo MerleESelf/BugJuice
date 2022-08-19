7b6aa7d314f0bff31cb9a8c2bd80aebfce1360a6
MerleESelf <merleself@gmail.com>
2022-08-18 20:10:35 -0400

    changes tp app

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,15 +1,11 @@
 import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import GitHubProvider from "next-auth/providers/github";
 
 const options = {
   providers: [
-    Providers.GitHub({
+    GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET
-    }),
-    Providers.Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
     })
   ],
 }
