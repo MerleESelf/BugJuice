eb70fe496ab3d229a31dcdd0066ff94acb53c833
MerleESelf <merleself@gmail.com>
2022-09-30 15:01:19 -0400

    further updates

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
 import NextAuth from 'next-auth'
 import GitHubProvider from "next-auth/providers/github";
-// import GoogleProvider from 'next-auth/providers/google';
 import SequelizeAdapter from "@next-auth/sequelize-adapter"
 import db from '../../../db';
 
@@ -11,20 +10,10 @@ const options = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET
     }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_ID,
-    //   clientSecret: process.env.GOOGLE_SECRET,
-    // })
+
   ],
   adapter: SequelizeAdapter(db),
   database: process.env.DATABASE,
-  // callbacks: {
-  //   async session({ session, token, user }) {
-  //     // Send properties to the client, like an access_token from a provider.
-  //     session.accessToken = token.accessToken
-  //     return session
-  //   }
-  // }
 }
 
 export default (req, res) => NextAuth(req, res, options)
