e69e05d0bdd154973d22ceb49d56106e791fb330
MerleESelf <merleself@gmail.com>
2023-01-05 18:48:24 -0500

    fixed get all todos issue

--- db/db.js
+++ db/db.js
@@ -1,25 +1,5 @@
 import { Sequelize } from "sequelize";
-import * as pg from 'pg';
-
-// const config = {
-//   host: process.env.HOST,
-//   database: process.env.DATABASE,
-//   username: process.env.DB_USER,
-//   dialect: "postgres",
-//   port: process.env.PORT,
-//   logging: false,
-// };
-
-// const prod_config = {
-//   host: process.env.PROD_HOST,
-//   database: process.env.PROD_DB,
-//   username: process.env.PROD_USER,
-//   password: process.env.PROD_PASSWORD,
-//   dialect: "postgres",
-//   dialectModule: pg,
-//   port: process.env.PROD_PORT,
-//   logging: false,
-// };
+import * as pg from 'pg'
 
 const db = new Sequelize(
   process.env.PROD_DB,
