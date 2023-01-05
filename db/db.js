f8b9021bf022fa318bc38226e15abfb87ac2fc75
MerleESelf <merleself@gmail.com>
2023-01-05 14:01:59 -0500

    possible solution to connection error

--- db/db.js
+++ db/db.js
@@ -1,25 +1,36 @@
 import { Sequelize } from "sequelize";
 import * as pg from 'pg';
 
-const config = {
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  username: process.env.DB_USER,
-  dialect: "postgres",
-  port: process.env.PORT,
-  logging: false,
-};
+// const config = {
+//   host: process.env.HOST,
+//   database: process.env.DATABASE,
+//   username: process.env.DB_USER,
+//   dialect: "postgres",
+//   port: process.env.PORT,
+//   logging: false,
+// };
 
-const prod_config = {
-  host: process.env.PROD_HOST,
-  database: process.env.PROD_DB,
-  username: process.env.PROD_USER,
-  password: process.env.PROD_PASSWORD,
-  dialect: "postgres",
-  dialectModule: pg,
-  port: process.env.PROD_PORT,
-  logging: false,
-};
+// const prod_config = {
+//   host: process.env.PROD_HOST,
+//   database: process.env.PROD_DB,
+//   username: process.env.PROD_USER,
+//   password: process.env.PROD_PASSWORD,
+//   dialect: "postgres",
+//   dialectModule: pg,
+//   port: process.env.PROD_PORT,
+//   logging: false,
+// };
 
-const db = new Sequelize(prod_config);
+const db = new Sequelize(
+  process.env.PROD_DB,
+  process.env.PROD_USER,
+  process.env.PROD_PASSWORD,
+  {
+    host: process.env.PROD_HOST,
+    port: process.env.PROD_PORT,
+    dialect: "postgres",
+    dialectModule: pg
+  }
+);
+
 export { db };
